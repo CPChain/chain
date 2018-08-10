6e86efb31e89feecf071ccadd0ef03ce8688f00d
Jason Carter <jsyqrt@gmail.com>
2018-08-10 12:01:32 +0800

    CHAIN-26 bug fix.

--- consensus/dpor/contract/campaign.sol
+++ consensus/dpor/contract/campaign.sol
@@ -1,8 +1,60 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.11;
+
+library Set {
+    // We define a new struct datatype that will be used to
+    // hold its data in the calling contract.
+    struct Data {
+        mapping(address => bool) flags;
+        address[] values;
+    }
+
+    // Note that the first parameter is of type "storage
+    // reference" and thus only its storage address and not
+    // its contents is passed as part of the call.  This is a
+    // special feature of library functions.  It is idiomatic
+    // to call the first parameter 'self', if the function can
+    // be seen as a method of that object.
+    function insert(Data storage self, address value)
+      public returns (bool)
+    {
+        if (self.flags[value])
+            return false; // already there
+        self.flags[value] = true;
+        self.values.push(value);
+        return true;
+    }
+
+    function remove(Data storage self, address value)
+       public returns (bool)
+    {
+        if (!self.flags[value])
+            return false; // not there
+        self.flags[value] = false;
+        for (uint i = 0 ; i < self.values.length ; i++){
+            if (self.values[i] == value){
+                delete self.values[i];
+            }
+        }
+        return true;
+    }
+
+    function contains(Data storage self, address value)
+       public view returns (bool)
+    {
+        return self.flags[value];
+    }
+
+    function getAll(Data storage self)
+       public view returns (address[])
+    {
+        return self.values;
+    }
+}
 
 // TODO this file exposes security holes.
 
 contract Campaign {
+    using Set for Set.Data;
 
     address owner;
     uint public viewIdx = 0;
@@ -16,11 +68,11 @@ contract Campaign {
     struct CandidateInfo {
         uint numOfCampaign;
         uint deposit;
-        uint timestamp;
+        uint startViewIdx;
     }
 
-    mapping(address => CandidateInfo) public candidates;
-    mapping(uint => address[]) public campaignSnapshots;
+    mapping(address => CandidateInfo) candidates;
+    mapping(uint => Set.Data) campaignSnapshots;
 
     constructor() public {
         owner = msg.sender;
@@ -28,25 +80,24 @@ contract Campaign {
     
     function ClaimCampaign(uint num_of_campaign, uint rpt) public payable {
         require(rpt >= minimumRpt, "too low rpt.");
-        require((num_of_campaign >= minimumNoc && num_of_campaign <= maximumNoc), "num of campaign out of range.");
         require(msg.value == baseDeposit * num_of_campaign, "wrong deposit value.");
 
         address candidate = msg.sender;
 
-        // require(candidates[candidate].numOfCampaign == 0, "already claimed campaign.");
-
         // set candidate info.
         if (candidates[candidate].numOfCampaign == 0){
-            candidates[candidate] = CandidateInfo(num_of_campaign, msg.value, block.timestamp);
+            require((num_of_campaign >= minimumNoc && num_of_campaign <= maximumNoc), "num of campaign out of range.");
+            candidates[candidate] = CandidateInfo(num_of_campaign, msg.value, viewIdx);
         } else {
+            require((candidates[candidate].numOfCampaign + num_of_campaign >= minimumNoc && candidates[candidate].numOfCampaign + num_of_campaign <= maximumNoc), "num of campaign out of range.");
             candidates[candidate].numOfCampaign += num_of_campaign;
             candidates[candidate].deposit += msg.value;
-            candidates[candidate].timestamp = block.timestamp;
+            candidates[candidate].startViewIdx = viewIdx;
         }
 
         // add candidate to campaignSnapshots.
-        for(uint i = viewIdx; i < viewIdx + num_of_campaign; i++) {
-            campaignSnapshots[i].push(candidate);
+        for(uint i = candidates[candidate].startViewIdx; i < candidates[candidate].startViewIdx + num_of_campaign; i++) {
+            campaignSnapshots[i].insert(candidate);
         }
     }
 
@@ -55,34 +106,31 @@ contract Campaign {
         address candidate = msg.sender;
         require(candidates[candidate].numOfCampaign > 0, "already quit campaign.");
 
-        candidates[candidate] = CandidateInfo(0, 0, block.timestamp);
+        candidates[candidate] = CandidateInfo(0, 0, 0);
 
         candidate.transfer(candidates[candidate].deposit);
 
         // remove candidate from current view snapshot
-        for(uint i = 0; i < campaignSnapshots[viewIdx].length; i++) {
-            if (campaignSnapshots[viewIdx][i] == candidate) {
-                // TODO delete element from array
-                // delete campaignSnapshots[viewIdx][candidate];
-            }
+        for(uint i = candidates[candidate].startViewIdx; i < candidates[candidate].numOfCampaign; i++) {
+            campaignSnapshots[i].remove(candidate);
         }
     }
 
     function CandidatesOf(uint view_idx) public view returns (address[]){
-        return campaignSnapshots[view_idx];
+        return campaignSnapshots[view_idx].values;
     }
 
     function CandidateInfoOf(address candidate) public view returns (uint, uint, uint) {
-        return (candidates[candidate].numOfCampaign, candidates[candidate].deposit, candidates[candidate].timestamp);
+        return (candidates[candidate].numOfCampaign, candidates[candidate].deposit, candidates[candidate].startViewIdx);
     }
 
     // TODO. add restriction that only last commissioner can call this with require statement.
     function ViewChange() public {
-        for(uint i = 0; i < campaignSnapshots[viewIdx].length; i++){
-            address candidate = campaignSnapshots[viewIdx][i];
+        for(uint i = 0; i < campaignSnapshots[viewIdx].values.length; i++){
+            address candidate = campaignSnapshots[viewIdx].values[i];
             if(candidates[candidate].deposit >= baseDeposit){
                 candidates[candidate].deposit -= baseDeposit;
-                campaignSnapshots[viewIdx][i].transfer(baseDeposit);
+                campaignSnapshots[viewIdx].values[i].transfer(baseDeposit);
             }
         }
         viewIdx = viewIdx + 1;
