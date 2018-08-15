b4991e892df4d5395fea68aad87ccf5edc6d5e2a
程鑫 <chengxin@sunallies.com>
2018-08-15 16:36:01 +0800

    CHAIN-39 Add unit test for IPFS database and a empty IPFS database implementation with several TODO: tasks.
    CHAIN-39 Add IPFS connection config item to default config.
    
    CHAIN-39 implement IPFS-based database(proxy) and make sure all unittests passed.
    
    CHAIN-39 format code
    
    CHAIN-39 Add dependencies of go-ipfs-api
    
    CHAIN-39 Fix a typo and imports

---
+++ ethdb/ipfs_database.go
@@ -0,0 +1,66 @@
+package ethdb
+
+import (
+	"bytes"
+
+	"github.com/ipfs/go-ipfs-api"
+)
+
+type IpfsDatabase struct {
+	url   string
+	shell *shell.Shell
+}
+
+func NewIpfsDb(url string) *IpfsDatabase {
+	s := shell.NewShell(url)
+	return NewIpfsDbWithShell(url, s)
+}
+
+func NewIpfsDbWithShell(url string, s *shell.Shell) *IpfsDatabase {
+	return &IpfsDatabase{
+		url:   url,
+		shell: s,
+	}
+}
+
+func (db *IpfsDatabase) Get(key []byte) ([]byte, error) {
+	k := string(key[:])
+	reader, err := db.shell.Cat(k)
+	if err != nil {
+		return nil, err
+	}
+
+	const bufsize = 1000
+	buf := make([]byte, bufsize)
+	ret := []byte{}
+	for {
+		n, _ := reader.Read(buf)
+		if n == 0 {
+			break
+		}
+
+		ret = append(ret, buf[:n]...)
+	}
+	return ret, nil
+}
+
+func (db *IpfsDatabase) Put(value []byte) ([]byte, error) {
+	reader := bytes.NewBuffer(value)
+	hash, err := db.shell.Add(reader)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return []byte(hash), nil
+	}
+}
+
+func (db *IpfsDatabase) Delete(key []byte) error {
+	// TODO: implement it
+	return nil
+}
+
+func (db *IpfsDatabase) Has(key []byte) (bool, error) {
+	// TODO: implement it
+	return false, nil
+}
---
+++ ethdb/ipfs_database_test.go
@@ -0,0 +1,66 @@
+package ethdb_test
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+type composer struct {
+	name      string
+	birthYear int
+}
+
+const testDbUrl = "localhost:5001"
+const testDbWrongUrl = "localhost:5002"
+
+var normalContent = []byte("this is a placeholder for private tx payload.")
+
+// Test for putting and getting normal content
+func TestIpfsDbGetPutWithNormalContent(t *testing.T) {
+	db := ethdb.NewIpfsDb(testDbUrl)
+	key, err := db.Put(normalContent)
+	if key == nil {
+		t.Errorf("Normal put operation should return a non-empty key.")
+	}
+	if err != nil {
+		t.Errorf("Normal put operation should succeed and return an nil error.")
+	}
+	retValue, err := db.Get(key)
+	if err != nil {
+		t.Errorf("Getting a successfully saved normal content should not return any error.")
+	}
+	if string(normalContent) != string(retValue) {
+		t.Errorf("Retrieved content %v does not equal to original value %v", retValue, normalContent)
+	}
+}
+
+// Test for putting and getting empty content
+func TestIpfsDbGetPutWithEmptyValue(t *testing.T) {
+	db := ethdb.NewIpfsDb(testDbUrl)
+	// putting empty value into db should work as normal
+	emptyContent := []byte{}
+
+	key, err := db.Put(emptyContent)
+	if key == nil {
+		t.Errorf("Putting empty content should return a non-empty key.")
+	}
+	if err != nil {
+		t.Errorf("Putting empty content should succeed and return an nil error.")
+	}
+	retValue, err := db.Get(key)
+	if err != nil {
+		t.Errorf("Getting a saved empty content should not return any error.")
+	}
+	if len(retValue) != 0 {
+		t.Errorf("The retrieved content should be empty.")
+	}
+}
+
+func TestIpfsDbWithWrongUrl(t *testing.T) {
+	wrongDb := ethdb.NewIpfsDb(testDbWrongUrl)
+	_, err := wrongDb.Put(normalContent)
+	if err == nil {
+		t.Errorf("Wrong url should cause an error.")
+	}
+}
