2420608eb057ed5e2b798e43de521436923431ab
Alex Cheng <chengx@cpchain.io>
2018-08-29 11:34:12 +0800

    CHAIN-39 Refactor the IPFS db code to allow weaving fake ipfs adapter for unit testing. In addition, I make some code optimization and add some comments.

--- ethdb/ipfs_database.go
+++ ethdb/ipfs_database.go
@@ -3,64 +3,59 @@ package ethdb
 import (
 	"bytes"
 
+	"io"
+
+	"io/ioutil"
+
 	"github.com/ipfs/go-ipfs-api"
 )
 
+// IpfsDatabase is a IPFS-based database.
 type IpfsDatabase struct {
-	url   string
-	shell *shell.Shell
+	adapter IpfsAdapter
 }
 
+// IpfsAdapter represents an adapter for IPFS access.
+// It also makes a room for weaving fake IPFS in unit test.
+type IpfsAdapter interface {
+	// Read data from IPFS.
+	Cat(path string) (io.ReadCloser, error)
+	// Add/save data to IPFS.
+	Add(r io.Reader) (string, error)
+}
+
+// NewIpfsDb creates a new IpfsDatabase instance with given url which is the IPFS node's API url.
 func NewIpfsDb(url string) *IpfsDatabase {
-	s := shell.NewShell(url)
-	return NewIpfsDbWithShell(url, s)
+	s := IpfsAdapter(shell.NewShell(url))
+	return NewIpfsDbWithAdapter(s)
 }
 
-func NewIpfsDbWithShell(url string, s *shell.Shell) *IpfsDatabase {
+// NewIpfsDbWithAdapter creates a new IpfsDatabase instance with given IPFS adapter.
+func NewIpfsDbWithAdapter(adapter IpfsAdapter) *IpfsDatabase {
 	return &IpfsDatabase{
-		url:   url,
-		shell: s,
+		adapter: adapter,
 	}
 }
 
+// Get retrieves data from IPFS with given key.
 func (db *IpfsDatabase) Get(key []byte) ([]byte, error) {
 	k := string(key[:])
-	reader, err := db.shell.Cat(k)
+	reader, err := db.adapter.Cat(k)
 	if err != nil {
 		return nil, err
 	}
 
-	const bufsize = 1000
-	buf := make([]byte, bufsize)
-	var ret []byte
-	for {
-		n, _ := reader.Read(buf)
-		if n == 0 {
-			break
-		}
-
-		ret = append(ret, buf[:n]...)
-	}
-	return ret, nil
+	return ioutil.ReadAll(reader)
 }
 
+// Put saves data to IPFS and return key(internally it is a hash).
 func (db *IpfsDatabase) Put(value []byte) ([]byte, error) {
 	reader := bytes.NewBuffer(value)
-	hash, err := db.shell.Add(reader)
+	hash, err := db.adapter.Add(reader)
 
 	if err != nil {
 		return nil, err
 	} else {
 		return []byte(hash), nil
 	}
-}
-
-func (db *IpfsDatabase) Delete(key []byte) error {
-	// TODO: implement it
-	return nil
-}
-
-func (db *IpfsDatabase) Has(key []byte) (bool, error) {
-	// TODO: implement it
-	return false, nil
 }
--- ethdb/ipfs_database_test.go
+++ ethdb/ipfs_database_test.go
@@ -3,22 +3,47 @@ package ethdb_test
 import (
 	"testing"
 
+	"bytes"
+	"crypto/sha256"
+	"io"
+	"io/ioutil"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethdb"
 )
 
-type composer struct {
-	name      string
-	birthYear int
+// FakeIpfsAdapter is a fake IPFS for unit test.
+type FakeIpfsAdapter struct {
+	store map[string][]byte
 }
 
-const testDbUrl = "localhost:5001"
-const testDbWrongUrl = "localhost:5002"
+// newFakeIpfsAdapter creates a new FakeIpfsAdapter instance.
+func newFakeIpfsAdapter() *FakeIpfsAdapter {
+	return &FakeIpfsAdapter{
+		store: map[string][]byte{},
+	}
+}
+
+func (adapter *FakeIpfsAdapter) Cat(path string) (io.ReadCloser, error) {
+	buf := adapter.store[path]
+	return ioutil.NopCloser(bytes.NewReader(buf)), nil
+}
+
+func (adapter *FakeIpfsAdapter) Add(r io.Reader) (string, error) {
+	data, _ := ioutil.ReadAll(r)
+	hash := sha256.Sum256(data)
+	path := hexutil.Encode(hash[:])
+	adapter.store[path] = data[:]
+	return path, nil
+}
+
+const testDbWrongURL = "localhost:5002"
 
 var normalContent = []byte("this is a placeholder for private tx payload.")
 
-// Test for putting and getting normal content
+// TestIpfsDbGetPutWithNormalContent tests for putting and getting normal content.
 func TestIpfsDbGetPutWithNormalContent(t *testing.T) {
-	db := ethdb.NewIpfsDb(testDbUrl)
+	db := ethdb.NewIpfsDbWithAdapter(newFakeIpfsAdapter())
 	key, err := db.Put(normalContent)
 	if key == nil {
 		t.Errorf("Normal put operation should return a non-empty key.")
@@ -35,9 +60,9 @@ func TestIpfsDbGetPutWithNormalContent(t *testing.T) {
 	}
 }
 
-// Test for putting and getting empty content
+// TestIpfsDbGetPutWithEmptyValue tests for putting and getting empty content.
 func TestIpfsDbGetPutWithEmptyValue(t *testing.T) {
-	db := ethdb.NewIpfsDb(testDbUrl)
+	db := ethdb.NewIpfsDbWithAdapter(newFakeIpfsAdapter())
 	// putting empty value into db should work as normal
 	emptyContent := []byte{}
 
@@ -57,8 +82,9 @@ func TestIpfsDbGetPutWithEmptyValue(t *testing.T) {
 	}
 }
 
+// TestIpfsDbWithWrongUrl tests new IPFS database with wrong URL.
 func TestIpfsDbWithWrongUrl(t *testing.T) {
-	wrongDb := ethdb.NewIpfsDb(testDbWrongUrl)
+	wrongDb := ethdb.NewIpfsDb(testDbWrongURL)
 	_, err := wrongDb.Put(normalContent)
 	if err == nil {
 		t.Errorf("Wrong url should cause an error.")
