a3ef51fcddd2757495b41452b8d6ca71ec10a9a6
xmx0632 <xumx@cpchain.io>
2018-09-17 15:21:26 +0800

    CHAIN-74 print receipt for debug

--- contracts/dpor/signerInfoRegister_test.go
+++ contracts/dpor/signerInfoRegister_test.go
@@ -17,31 +17,54 @@
 package dpor
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"fmt"
 
 	"math/big"
 	"testing"
 
+	"log"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/contracts/dpor/contract"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 func deployRegister(prvKey *ecdsa.PrivateKey, amount *big.Int, backend *backends.SimulatedBackend) (common.Address, error) {
 	deployTransactor := bind.NewKeyedTransactor(prvKey)
 	deployTransactor.Value = amount
-	addr, _, _, err := contract.DeploySignerConnectionRegister(deployTransactor, backend)
+	deployTransactor.GasLimit = 3000000
+	addr, tx, _, err := contract.DeploySignerConnectionRegister(deployTransactor, backend)
 	if err != nil {
+		log.Fatalf("failed to deploy contact when mining :%v", err)
 		return common.Address{}, err
 	}
+
+	_ = tx
 	backend.Commit()
+
+	printReceipt(backend, tx, "ReceiptStatusFailed when DeploySignerConnectionRegister:%v")
 	return addr, nil
 }
 
+func printReceipt(backend *backends.SimulatedBackend, tx *types.Transaction, msg string) {
+	ctx := context.Background()
+	receipt, err := backend.TransactionReceipt(ctx, tx.Hash())
+	if err != nil {
+		log.Fatalf(msg, err)
+	}
+	fmt.Println("receipt.Status:", receipt.Status)
+	if receipt.Status == types.ReceiptStatusFailed {
+		log.Fatalf(msg, receipt.Status)
+	}
+}
+
 func TestDeploySignerConnectionRegister(t *testing.T) {
+
 	contractBackend := backends.NewDporSimulatedBackend(core.GenesisAlloc{addr: {Balance: big.NewInt(1000000000000)}})
 	contractAddr, err := deployRegister(key, big.NewInt(0), contractBackend)
 	checkError(t, "deploy contract: expected no error, got %v", err)
@@ -52,31 +75,39 @@ func TestDeploySignerConnectionRegister(t *testing.T) {
 	_ = contractAddr
 	contractBackend.Commit()
 
-	// ==================================================
+	// ==============RegisterPublicKey====================
 	register.TransactOpts = *bind.NewKeyedTransactor(key)
 	register.TransactOpts.GasLimit = 100000
 	register.TransactOpts.GasPrice = big.NewInt(0)
 	register.TransactOpts.Value = big.NewInt(1150)
-	tx, err := register.AddNodeInfo(big.NewInt(1), addr, []byte("777"))
-	checkError(t, "nodeInfo error: %v", err)
-	contractBackend.Commit()
-
-	// ==================================================
-	nodeInfoBytes, err := register.GetNodeInfo(big.NewInt(1), addr)
-	checkError(t, "nodeInfo error: %v", err)
-	fmt.Println("nodeInfoBytes:", string(nodeInfoBytes))
-	contractBackend.Commit()
-
-	// =================================================
 	var testme = "testmme"
-	tx, err = register.RegisterPublicKey([]byte(testme))
+	tx, err := register.RegisterPublicKey([]byte(testme))
 	fmt.Println("RegisterPublicKey tx:", tx.Hash().Hex())
 	contractBackend.Commit()
-	// =================================================
+
+	printReceipt(contractBackend, tx, "ReceiptStatusFailed when RegisterPublicKey:%v")
+
+	// ============GetPublicKey====================
 	publicKey, err := register.GetPublicKey(addr)
 	checkError(t, "publicKey error: %v", err)
 	fmt.Println("publicKey:", publicKey, string(publicKey))
 	if testme != string(publicKey) {
 		t.Errorf("public key error,want:%v,got:%v", testme, string(publicKey))
 	}
+
+	// ============AddNodeInfo========================
+	register.TransactOpts = *bind.NewKeyedTransactor(key)
+	register.TransactOpts.GasLimit = 100000
+	register.TransactOpts.GasPrice = big.NewInt(0)
+	register.TransactOpts.Value = big.NewInt(1150)
+	tx, err = register.AddNodeInfo(big.NewInt(1), addr, []byte("777"))
+	checkError(t, "AddNodeInfo error: %v", err)
+	contractBackend.Commit()
+
+	printReceipt(contractBackend, tx, "ReceiptStatusFailed when AddNodeInfo:%v")
+	// ==============GetNodeInfo=========================
+	nodeInfoBytes, err := register.GetNodeInfo(big.NewInt(1), addr)
+	checkError(t, "GetNodeInfo error: %v", err)
+	fmt.Println("GetNodeInfo:", string(nodeInfoBytes))
+	contractBackend.Commit()
 }
