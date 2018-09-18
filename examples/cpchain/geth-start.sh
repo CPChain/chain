4cfe5e2caa7d661115be3785f97f8b30f5497449
Alex Cheng <chengx@cpchain.io>
2018-09-18 20:59:38 +0800

    CHAIN-80 revert an inadvertently changed file.

--- examples/cpchain/geth-start.sh
+++ examples/cpchain/geth-start.sh
@@ -21,7 +21,7 @@ $proj_dir/build/bin/bootnode -nodekey boot.key -verbosity 9 -addr :30310 2>data/
 
 data_dir=`pwd`
 
-#$proj_dir/build/bin/geth $ARGS --datadir $data_dir/data/dd1 --ipcpath ${ipc_path_base}1 --rpcport 8501 --port 30311 --unlock "0xe94b7b6c5a0e526a4d97f9768ad6097bde25c62a" --mine --minerthreads 1 --cpchain --password conf/password 2>data/logs/1.log &
+$proj_dir/build/bin/geth $ARGS --datadir $data_dir/data/dd1 --ipcpath ${ipc_path_base}1 --rpcport 8501 --port 30311 --unlock "0xe94b7b6c5a0e526a4d97f9768ad6097bde25c62a" --mine --minerthreads 1 --cpchain --password conf/password 2>data/logs/1.log &
 $proj_dir/build/bin/geth $ARGS --datadir $data_dir/data/dd2 --ipcpath ${ipc_path_base}2 --rpcport 8502 --port 30312 --unlock "0xc05302acebd0730e3a18a058d7d1cb1204c4a092" --mine --minerthreads 1 --cpchain --password conf/password 2>data/logs/2.log &
 $proj_dir/build/bin/geth $ARGS --datadir $data_dir/data/dd3 --ipcpath ${ipc_path_base}3 --rpcport 8503 --port 30313 --unlock "0xef3dd127de235f15ffb4fc0d71469d1339df6465" --mine --minerthreads 1 --cpchain --password conf/password1 2>data/logs/3.log &
 $proj_dir/build/bin/geth $ARGS --datadir $data_dir/data/dd4 --ipcpath ${ipc_path_base}4 --rpcport 8504 --port 30314 --unlock "0x3a18598184ef84198db90c28fdfdfdf56544f747"  --cpchain --password conf/password2 2>data/logs/4.log &
