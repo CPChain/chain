21450f561844b6a59395be04b8527c3b2d9defac
Alex Cheng <chengx@cpchain.io>
2018-09-27 14:58:29 +0800

    CHAIN-105 load RSA key instead of hardcode.

--- private/payload_retriever.go
+++ private/payload_retriever.go
@@ -9,8 +9,15 @@ import (
 	"encoding/binary"
 
 	"bitbucket.org/cpchain/chain/common/hexutil"
+	"bitbucket.org/cpchain/chain/crypto/rsa_"
 	"bitbucket.org/cpchain/chain/ethdb"
+	"bitbucket.org/cpchain/chain/log"
 	"bitbucket.org/cpchain/chain/rlp"
+	"github.com/pkg/errors"
+)
+
+var (
+	cannotObtainRSAKey = errors.New("Cannot obtain RSA key.")
 )
 
 // Read tx's payload replacement, retrieve encrypted payload from IPFS and decrypt it.
@@ -22,8 +29,11 @@ func RetrieveAndDecryptPayload(data []byte, txNonce uint64, remoteDB ethdb.Remot
 		return []byte{}, false, err
 	}
 
-	prv := getKey()
-	pub := hexutil.Encode(x509.MarshalPKCS1PublicKey(&prv.PublicKey))
+	pub, prv, err := getKey()
+	if err != nil {
+		return nil, false, err
+	}
+	pubEncoded := hexutil.Encode(x509.MarshalPKCS1PublicKey(pub))
 
 	// Check if the current node is in the participant group by comparing is public key and decrypt with its private
 	// key and return result.
@@ -36,7 +46,7 @@ func RetrieveAndDecryptPayload(data []byte, txNonce uint64, remoteDB ethdb.Remot
 	for i, k := range replacement.Participants {
 		encryptedKey := sp.SymmetricKeys[i]
 		// If the participant's public key equals to current public key, decrypt with corresponding private key.
-		if k == pub {
+		if k == pubEncoded {
 			symKey := decryptSymKey(encryptedKey, prv)
 			decrypted, _ := decryptPayload(sp.Payload, symKey, txNonce)
 			return decrypted, true, nil
@@ -54,9 +64,15 @@ func getDataFromRemote(ipfsAddress []byte, remoteDB ethdb.RemoteDatabase) []byte
 	return content
 }
 
-func getKey() *rsa.PrivateKey {
-	// TODO: replace the hardcoded stuff with real code.
-	return GetPrivateKeyForAccount("c05302acebd0730e3a18a058d7d1cb1204c4a092")
+// TODO: 1. figure out the performance due to I/O; 2. merge with node/config.go/RsaKey() function.
+func getKey() (*rsa.PublicKey, *rsa.PrivateKey, error) {
+	rsaPubPath := "./rsa/rsa_pub.pem"
+	rsaPriPath := "./rsa/rsa_pri.pem"
+	if pubKey, priKey, _, _, err := rsa_.LoadRsaKey(rsaPubPath, rsaPriPath); err == nil {
+		log.Info("Obtain private/public RSA keypair.", hexutil.Encode(x509.MarshalPKCS1PublicKey(pubKey)))
+		return pubKey, priKey, nil
+	}
+	return nil, nil, cannotObtainRSAKey
 }
 
 func decryptSymKey(data []byte, privateKey *rsa.PrivateKey) []byte {
