6a3ba833bea6d82dc291e52fa2b4bdde4089a466
Alex Cheng <chengx@cpchain.io>
2018-10-08 11:33:39 +0800

    CHAIN-105 fix lint error

--- core/private_database_test.go
+++ core/private_database_test.go
@@ -11,8 +11,8 @@ import (
 	"bitbucket.org/cpchain/chain/core/types"
 	"bitbucket.org/cpchain/chain/crypto/sha3"
 	"bitbucket.org/cpchain/chain/ethdb"
-	"bitbucket.org/cpchain/chain/rlp"
 	"bitbucket.org/cpchain/chain/trie"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/pkg/errors"
 )
 
