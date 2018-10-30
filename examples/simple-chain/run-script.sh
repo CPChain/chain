f0f76f03fdac238179b91b8bba558c3f453a24f6
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-10-30 19:24:06 +0800

    trim examples

--- examples/simple-chain/run-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-scriptfile="./transactions/$1"
-
-proj_dir=../..
-
-$proj_dir/build/bin/cpchain --exec "loadScript(\"$scriptfile\")" attach ipc:data/dd1/cpchain.ipc
