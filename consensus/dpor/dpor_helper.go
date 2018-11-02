7ffe4ef65d2a90af3f2d74db00e238607e1080b2
Liu Qian <liuqian@cpchain.io>
2018-11-02 17:08:15 +0800

    CHAIN-239 added signheader to dpor_helper.

--- consensus/dpor/dpor_helper.go
+++ consensus/dpor/dpor_helper.go
@@ -14,10 +14,11 @@ import (
 
 type dporHelper interface {
 	dporUtil
-	verifyHeader(c *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
-	verifyCascadingFields(c *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
-	snapshot(c *Dpor, chain consensus.ChainReader, number uint64, hash common.Hash, parents []*types.Header) (*DporSnapshot, error)
-	verifySeal(c *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
+	verifyHeader(d *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
+	verifyCascadingFields(d *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
+	snapshot(d *Dpor, chain consensus.ChainReader, number uint64, hash common.Hash, parents []*types.Header) (*DporSnapshot, error)
+	verifySeal(d *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error
+	// signHeader(d *Dpor, chain consensus.ChainReader, header *types.Header) error
 }
 
 type defaultDporHelper struct {
@@ -310,14 +311,14 @@ func (dh *defaultDporHelper) verifySeal(dpor *Dpor, chain consensus.ChainReader,
 }
 
 // sighHeader signs the given refHeader if self is in the committee
-func (dh *defaultDporHelper) signHeader(dpor *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) (*types.Header, error) {
+func (dh *defaultDporHelper) signHeader(dpor *Dpor, chain consensus.ChainReader, header *types.Header) error {
 	hash := header.Hash()
 	number := header.Number.Uint64()
 
 	// Retrieve the Snapshot needed to verify this header and cache it
-	snap, err := dh.snapshot(dpor, chain, number-1, header.ParentHash, parents)
+	snap, err := dh.snapshot(dpor, chain, number-1, header.ParentHash, nil)
 	if err != nil {
-		return header, err
+		return err
 	}
 
 	// Retrieve signatures of the block in cache
@@ -332,9 +333,9 @@ func (dh *defaultDporHelper) signHeader(dpor *Dpor, chain consensus.ChainReader,
 	}
 
 	// Encode allSigs to header.extra2.
-	err = refHeader.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
+	err = header.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
 	if err != nil {
-		return header, err
+		return err
 	}
 
 	// Sign the block if self is in the committee
@@ -342,13 +343,13 @@ func (dh *defaultDporHelper) signHeader(dpor *Dpor, chain consensus.ChainReader,
 
 		// NOTE: sign a block only once
 		if signedHash, signed := dpor.signedBlocks[header.Number.Uint64()]; signed && signedHash != header.Hash() {
-			return header, errMultiBlocksInOneHeight
+			return errMultiBlocksInOneHeight
 		}
 
 		// Sign it!
 		sighash, err := dpor.signFn(accounts.Account{Address: dpor.signer}, dpor.dh.sigHash(header).Bytes())
 		if err != nil {
-			return header, err
+			return err
 		}
 
 		// Copy signer's signature to the right position in the allSigs
@@ -356,16 +357,15 @@ func (dh *defaultDporHelper) signHeader(dpor *Dpor, chain consensus.ChainReader,
 		copy(allSigs[round*extraSeal:(round+1)*extraSeal], sighash)
 
 		// Encode to header.extra2
-		err = refHeader.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
+		err = header.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
 		if err != nil {
-			return header, err
+			return err
 		}
 
 		// Return special err to return new signed header
-		return refHeader, nil
+		return nil
 	}
-	return header, nil
-
+	return nil
 }
 
 // timeToDialCommittee checks if it is time to dial remote signers, and dails them if time is up
@@ -419,181 +419,3 @@ func (dh *defaultDporHelper) dialCommittee(dpor *Dpor, snap *DporSnapshot, numbe
 
 	return nil
 }
-
-// // verifySeal checks whether the signature contained in the header satisfies the
-// // consensus protocol requirements. The method accepts an optional list of parent
-// // headers that aren't yet part of the local blockchain to generate the snapshots
-// // from.
-// func (dh *defaultDporHelper) verifySeal(dpor *Dpor, chain consensus.ChainReader, header *types.Header, parents []*types.Header, refHeader *types.Header) error {
-// 	hash := header.Hash()
-// 	number := header.Number.Uint64()
-
-// 	// Verifying the genesis block is not supported
-// 	if number == 0 {
-// 		return errUnknownBlock
-// 	}
-
-// 	// TODO: @liuq fix this!!!
-// 	if dpor.fake == FakeMode || dpor.fake == DoNothingFakeMode {
-// 		time.Sleep(dpor.fakeDelay)
-// 		if dpor.fakeFail == number {
-// 			return errFakerFail
-// 		}
-// 		return nil
-// 	}
-
-// 	// Retrieve the Snapshot needed to verify this header and cache it
-// 	snap, err := dh.snapshot(dpor, chain, number-1, header.ParentHash, parents)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// Resolve the authorization key and check against signers
-// 	leader, signers, err := dpor.dh.ecrecover(header, dpor.signatures)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// Some debug infos here
-// 	log.Debug("--------dpor.verifySeal start--------")
-
-// 	log.Debug("hash", "hash", hash.Hex())
-
-// 	log.Debug("number", "number", number)
-// 	log.Debug("current header", "number", chain.CurrentHeader().Number.Uint64())
-
-// 	log.Debug("leader", "address", leader.Hex())
-
-// 	log.Debug("signers recoverd from header: ")
-// 	for _, signer := range signers {
-// 		log.Debug("signer", "address", signer.Hex())
-// 	}
-
-// 	log.Debug("signers in snapshot: ")
-// 	for _, signer := range snap.SignersOf(number) {
-// 		log.Debug("signer", "address", signer.Hex())
-// 	}
-
-// 	// Check if the leader is the real leader
-// 	ok, err := snap.IsLeaderOf(leader, number)
-// 	if err != nil {
-// 		log.Warn("err in snapshot.IsLeaderOf", "err", err)
-// 		return err
-// 	}
-
-// 	// If leader is a wrong leader, return err
-// 	if !ok {
-// 		return consensus.ErrUnauthorized
-// 	}
-
-// 	// Check if accept the sigs and if leader is in the sigs
-// 	accept, err := dpor.dh.acceptSigs(header, dpor.signatures, snap.SignersOf(number), uint(dpor.config.Epoch))
-// 	if err != nil {
-// 		log.Warn("err in dpor.dh.acceptSigs", "err", err)
-// 		return err
-// 	}
-
-// 	// Retrieve signatures of the block in cache
-// 	s, _ := dpor.signatures.Get(hash)
-
-// 	// Copy all signatures recovered to allSigs.
-// 	allSigs := make([]byte, int(dpor.config.Epoch)*extraSeal)
-// 	for round, signer := range snap.SignersOf(number) {
-// 		if sigHash, ok := s.(*Signatures).GetSig(signer); ok {
-// 			copy(allSigs[round*extraSeal:(round+1)*extraSeal], sigHash)
-// 		}
-// 	}
-
-// 	// Encode allSigs to header.extra2.
-// 	err = refHeader.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// We haven't reached the 2/3 rule
-// 	if !accept {
-// 		// Sign the block if self is in the committee
-// 		if snap.IsSignerOf(dpor.signer, number) {
-
-// 			// NOTE: sign a block only once
-// 			if signedHash, signed := dpor.signedBlocks[header.Number.Uint64()]; signed && signedHash != header.Hash() {
-// 				return errMultiBlocksInOneHeight
-// 			}
-
-// 			// Sign it!
-// 			sighash, err := dpor.signFn(accounts.Account{Address: dpor.signer}, dpor.dh.sigHash(header).Bytes())
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			// Copy signer's signature to the right position in the allSigs
-// 			round, _ := snap.SignerRoundOf(dpor.signer, number)
-// 			copy(allSigs[round*extraSeal:(round+1)*extraSeal], sighash)
-
-// 			// Encode to header.extra2
-// 			err = refHeader.EncodeToExtra2(types.Extra2Struct{Type: types.TypeExtra2Signatures, Data: allSigs})
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			// Return special err to return new signed header
-// 			return consensus.ErrNewSignedHeader
-// 		}
-
-// 		// Not signer, return err
-// 		return consensus.ErrNotEnoughSigs
-
-// 	}
-// 	// --- our check ends ---
-
-// 	// Ensure that the difficulty corresponds to the turn-ness of the signer
-// 	inturn, _ := snap.IsLeaderOf(leader, header.Number.Uint64())
-// 	if inturn && header.Difficulty.Cmp(diffInTurn) != 0 {
-// 		return errInvalidDifficulty
-// 	}
-// 	if !inturn && header.Difficulty.Cmp(diffNoTurn) != 0 {
-// 		return errInvalidDifficulty
-// 	}
-
-// 	currentNum := chain.CurrentHeader().Number.Uint64()
-// 	number = currentNum
-
-// 	// Some debug infos
-// 	log.Debug("my address", "eb", dpor.signer.Hex())
-// 	log.Debug("ready to accept this block", "number", number)
-// 	log.Debug("current block number", "number", currentNum)
-// 	log.Debug("ISCheckPoint", "bool", IsCheckPoint(number, dpor.config.Epoch, dpor.config.View))
-// 	log.Debug("is future signer", "bool", snap.IsFutureSignerOf(dpor.signer, number))
-// 	log.Debug("epoch idx of block number", "block epochIdx", snap.EpochIdxOf(number))
-
-// 	log.Debug("recent signers: ")
-// 	for i := snap.EpochIdxOf(number); i < snap.EpochIdxOf(number)+5; i++ {
-// 		log.Debug("----------------------")
-// 		log.Debug("signers in snapshot of:", "epoch idx", i)
-// 		for _, s := range snap.RecentSigners[i] {
-// 			log.Debug("signer", "s", s.Hex())
-// 		}
-// 	}
-
-// 	log.Debug("--------dpor.verifySeal end--------")
-
-// 	// If in a checkpoint and self is in the future committee, try to build the committee network
-// 	if IsCheckPoint(number, dpor.config.Epoch, dpor.config.View) && number >= dpor.config.MaxInitBlockNumber && snap.IsFutureSignerOf(dpor.signer, number) {
-// 		log.Info("In future committee, building the committee network...")
-
-// 		epochIdx := snap.FutureEpochIdxOf(number)
-// 		signers := snap.FutureSignersOf(number)
-
-// 		go func(eIdx uint64, committee []common.Address) {
-// 			// Updates committeeNetworkHandler.RemoteSigners
-// 			dpor.committeeNetworkHandler.UpdateRemoteSigners(eIdx, committee)
-// 			// Connect all
-// 			dpor.committeeNetworkHandler.DialAll()
-// 		}(epochIdx, signers)
-
-// 	} else {
-// 		log.Info("Not in future committee, doing nothing.")
-// 	}
-
-// 	return nil
-// }
