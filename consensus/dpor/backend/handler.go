c2996f45fd40746d4456f12e9005de32a4c37e50
Liu Qian <liuqian@cpchain.io>
2018-11-05 16:07:37 +0800

    CHAIN-239 added pbft handler.

--- consensus/dpor/backend/handler.go
+++ consensus/dpor/backend/handler.go
@@ -2,7 +2,6 @@ package backend
 
 import (
 	"context"
-	"encoding/hex"
 	"math/big"
 	"sync"
 
@@ -10,273 +9,67 @@ import (
 	"bitbucket.org/cpchain/chain/commons/crypto/rsakey"
 	"bitbucket.org/cpchain/chain/commons/log"
 	"bitbucket.org/cpchain/chain/configs"
-	"bitbucket.org/cpchain/chain/consensus"
-	"bitbucket.org/cpchain/chain/contracts/dpor/contracts/signerRegister"
+	contract "bitbucket.org/cpchain/chain/contracts/dpor/contracts/signerRegister"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/discover"
 )
 
-// Signer represents a remote signer waiting to be connected and communicate with.
-type Signer struct {
-	epochIdx      uint64
-	pubkey        []byte
-	nodeID        string
-	address       common.Address
-	pubkeyFetched bool
-	nodeIDFetched bool
-	nodeIDUpdated bool // bool to show if i updated my nodeid encrypted with this signer's pubkey to the contract.
-	dialed        bool // bool to show if i already connected to this signer.
-
-	lock sync.RWMutex
-}
-
-// NewSigner creates a new NewSigner with given view idx and address.
-func NewSigner(epochIdx uint64, address common.Address) *Signer {
-	return &Signer{
-		epochIdx: epochIdx,
-		address:  address,
-	}
-}
-
-// fetchPubkey fetches the public key of the remote signer from the contract.
-func (rs *Signer) fetchPubkey(contractInstance *contract.SignerConnectionRegister) error {
-
-	address := rs.address
-
-	log.Debug("fetching public key of remote signer")
-	log.Debug("signer", "addr", address)
-
-	pubkey, err := contractInstance.GetPublicKey(nil, address)
-	if err != nil {
-		return err
-	}
-
-	rs.pubkey = pubkey
-	rs.pubkeyFetched = true
-
-	log.Debug("fetched public key of remote signer", "pubkey", pubkey)
-
-	return nil
-}
-
-// fetchNodeID fetches the node id of the remote signer encrypted with my public key, and decrypts it with my private key.
-func (rs *Signer) fetchNodeID(contractInstance *contract.SignerConnectionRegister, rsaKey *rsakey.RsaKey) error {
-	epochIdx, address := rs.epochIdx, rs.address
-
-	log.Debug("fetching nodeID of remote signer")
-	log.Debug("epoch", "idx", epochIdx)
-	log.Debug("signer", "addr", address.Hex())
-
-	encryptedNodeID, err := fetchNodeID(epochIdx, address, contractInstance)
-	nodeid, err := rsaKey.RsaDecrypt(encryptedNodeID)
-	if err != nil {
-		log.Debug("encryptedNodeID")
-		log.Debug(hex.Dump(encryptedNodeID))
-		log.Debug("my pubkey")
-		log.Debug(hex.Dump(rsaKey.PublicKey.RsaPublicKeyBytes))
-		log.Debug("privKey", "privKey", rsaKey.PrivateKey)
-		return err
-	}
-
-	nodeID := string(nodeid)
-	rs.nodeID = nodeID
-	rs.nodeIDFetched = true
-
-	log.Debug("fetched nodeID of remote signer", "nodeID", nodeID)
-
-	return nil
-}
-
-func fetchNodeID(epochIdx uint64, address common.Address, contractInstance *contract.SignerConnectionRegister) ([]byte, error) {
-	encryptedNodeID, err := contractInstance.GetNodeInfo(nil, big.NewInt(int64(epochIdx)), address)
-	if err != nil {
-		return nil, err
-	}
-	return encryptedNodeID, nil
-}
-
-// updateNodeID encrypts my node id with this remote signer's public key and update to the contract.
-func (rs *Signer) updateNodeID(nodeID string, auth *bind.TransactOpts, contractInstance *contract.SignerConnectionRegister, client consensus.ClientBackend) error {
-	epochIdx, address := rs.epochIdx, rs.address
-
-	log.Debug("fetched rsa pubkey")
-	log.Debug(hex.Dump(rs.pubkey))
-
-	pubkey, err := rsakey.NewRsaPublicKey(rs.pubkey)
-
-	log.Debug("updating self nodeID with remote signer's public key")
-	log.Debug("epoch", "idx", epochIdx)
-	log.Debug("signer", "addr", address.Hex())
-	log.Debug("nodeID", "nodeID", nodeID)
-	log.Debug("pubkey", "pubkey", pubkey)
-
-	if err != nil {
-		return err
-	}
-
-	encryptedNodeID, err := pubkey.RsaEncrypt([]byte(nodeID))
-
-	log.Debug("encryptedNodeID")
-	log.Debug(hex.Dump(encryptedNodeID))
-
-	transaction, err := contractInstance.AddNodeInfo(auth, big.NewInt(int64(epochIdx)), address, encryptedNodeID)
-	if err != nil {
-		return err
-	}
-
-	ctx := context.Background()
-	_, err = bind.WaitMined(ctx, client, transaction)
-	if err != nil {
-		return err
-	}
-
-	rs.nodeIDUpdated = true
-
-	log.Debug("updated self nodeID")
-
-	return nil
-}
-
-// dial dials the signer.
-func (rs *Signer) dial(server *p2p.Server, nodeID string, address common.Address, auth *bind.TransactOpts, contractInstance *contract.SignerConnectionRegister, client consensus.ClientBackend, rsaKey *rsakey.RsaKey) (bool, error) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-
-	log.Debug("dialing to remote signer", "signer", rs)
-
-	// fetch remtoe signer's public key if there is no one.
-	if !rs.pubkeyFetched {
-		err := rs.fetchPubkey(contractInstance)
-		if err != nil {
-			log.Warn("err when fetching signer's pubkey from contract", "err", err)
-			return false, err
-		}
-	}
-
-	nodeid, err := fetchNodeID(rs.epochIdx, address, contractInstance)
-	if err != nil {
-		return false, err
-	}
-
-	// update my nodeID to contract if already know the public key of the remote signer and not updated yet.
-	if rs.pubkeyFetched && len(nodeid) == 0 {
-		err := rs.updateNodeID(nodeID, auth, contractInstance, client)
-		if err != nil {
-			log.Warn("err when updating my node id to contract", "err", err)
-			return false, err
-		}
-	}
-
-	// fetch the nodeID of the remote signer if not fetched yet.
-	if !rs.nodeIDFetched {
-		err := rs.fetchNodeID(contractInstance, rsaKey)
-		if err != nil {
-			log.Warn("err when fetching signer's nodeID from contract", "err", err)
-			return false, err
-		}
-	}
-
-	// dial the signer with his nodeID if not dialed yet.
-	if rs.nodeIDFetched && !rs.dialed {
-		node, err := discover.ParseNode(rs.nodeID)
-		if err != nil {
-			log.Warn("err when dialing remote signer with his nodeID", "err", err)
-			return false, err
-		}
-		if server != nil {
-			server.AddPeer(node)
-		} else {
-			log.Warn("invalid server", "server", server)
-		}
-		rs.dialed = true
-	}
-
-	return rs.dialed, nil
-}
-
-func (rs *Signer) Dial(server *p2p.Server, nodeID string, address common.Address, auth *bind.TransactOpts, contractInstance *contract.SignerConnectionRegister, client consensus.ClientBackend, rsaKey *rsakey.RsaKey) error {
-
-	succeed, err := rs.dial(server, nodeID, address, auth, contractInstance, client, rsaKey)
-	// succeed, err := func() (bool, error) { return true, nil }()
-
-	log.Debug("result of rs.dial", "succeed", succeed)
-	log.Debug("result of rs.dial", "err", err)
-
-	if succeed || err == nil {
-		return nil
-	}
-	return nil
-}
-
-func (rs *Signer) disconnect(server *p2p.Server) error {
-	rs.lock.Lock()
-	nodeID := rs.nodeID
-	rs.lock.Unlock()
-
-	node, err := discover.ParseNode(nodeID)
-	if err != nil {
-		return err
-	}
-	server.RemovePeer(node)
-	return nil
-}
-
-// BasicCommitteeHandler implements consensus.CommitteeHandler
-type BasicCommitteeHandler struct {
-	epochIdx uint64
+// Handler implements PbftHandler
+type Handler struct {
+	connected bool
+	epochIdx  uint64
 
 	ownNodeID  string
 	ownAddress common.Address
 
 	server *p2p.Server
+	RsaKey *rsakey.RsaKey
 
 	contractAddress    common.Address
-	contractCaller     *consensus.ContractCaller
+	contractCaller     *ContractCaller
 	contractInstance   *contract.SignerConnectionRegister
 	contractTransactor *bind.TransactOpts
 
-	RsaKey *rsakey.RsaKey
+	signers map[common.Address]*Signer
 
-	remoteSigners []*Signer
-
-	connected bool
-	lock      sync.RWMutex
+	lock sync.RWMutex
 }
 
-// NewBasicCommitteeNetworkHandler creates a BasicCommitteeNetworkHandler instance
-func NewBasicCommitteeNetworkHandler(config *configs.DporConfig, etherbase common.Address) (*BasicCommitteeHandler, error) {
-	ch := &BasicCommitteeHandler{
+// New creates a PbftHandler
+func New(config *configs.DporConfig, etherbase common.Address) (*Handler, error) {
+	ch := &Handler{
 		ownAddress:      etherbase,
 		contractAddress: config.Contracts["signer"],
-		remoteSigners:   make([]*Signer, config.Epoch),
-		// remoteSigners:   make([]*Signer, config.Epoch-1),
-		connected: false,
+		signers:         make(map[common.Address]*Signer),
+		connected:       false,
 	}
 	return ch, nil
 }
 
-func (ch *BasicCommitteeHandler) UpdateServer(server *p2p.Server) error {
+// SetServer sets handler.server
+func (ch *Handler) SetServer(server *p2p.Server) error {
 	ch.lock.Lock()
 	defer ch.lock.Unlock()
+
 	ch.server = server
 	ch.ownNodeID = server.Self().String()
+
 	return nil
 }
 
-func (ch *BasicCommitteeHandler) SetRSAKeys(rsaReader RSAReader) error {
+// SetRsaKey sets handler.rsaKey
+func (ch *Handler) SetRsaKey(rsaReader RsaReader) error {
 	ch.lock.Lock()
 	defer ch.lock.Unlock()
 
 	var err error
-
 	ch.RsaKey, err = rsaReader()
 
 	return err
 }
 
-// UpdateContractCaller updates contractcaller.
-func (ch *BasicCommitteeHandler) UpdateContractCaller(contractCaller *consensus.ContractCaller) error {
+// SetContractCaller sets handler.contractcaller.
+func (ch *Handler) SetContractCaller(contractCaller *ContractCaller) error {
 
 	// creates an contract instance
 	contractInstance, err := contract.NewSignerConnectionRegister(ch.contractAddress, contractCaller.Client)
@@ -308,48 +101,39 @@ func (ch *BasicCommitteeHandler) UpdateContractCaller(contractCaller *consensus.
 	return nil
 }
 
-// UpdateSigners updates BasicCommitteeNetworkHandler's remoteSigners.
-func (ch *BasicCommitteeHandler) UpdateSigners(epochIdx uint64, signers []common.Address) error {
+// UpdateSigners updates Handler's signers.
+func (ch *Handler) UpdateSigners(epochIdx uint64, signers []common.Address) error {
 	ch.lock.Lock()
-	remoteSigners := ch.remoteSigners
+	remoteSigners := ch.signers
 	ch.lock.Unlock()
 
-	log.Debug("remote signers", "signers", signers)
-	log.Debug("ch.remoteSigners", "signers", remoteSigners)
-
-	for i, signer := range signers {
-		// if remoteSigners[i] == nil || remoteSigners[i].address != signer {
-		if remoteSigners[i] == nil {
-
-			// // omit self
-			// if signer == ch.contractTransactor.From {
-			// 	continue
-			// }
+	for _, signer := range signers {
+		if _, ok := remoteSigners[signer]; !ok {
 			s := NewSigner(epochIdx, signer)
-			remoteSigners[i] = s
+			remoteSigners[signer] = s
 		}
 	}
 
 	ch.lock.Lock()
 	ch.epochIdx = epochIdx
-	ch.remoteSigners = remoteSigners
+	ch.signers = remoteSigners
 	ch.lock.Unlock()
 
 	return nil
 }
 
 // DialAll connects remote signers.
-func (ch *BasicCommitteeHandler) DialAll() {
+func (ch *Handler) DialAll() {
 	ch.lock.Lock()
 	nodeID, address, contractInstance, auth, client := ch.ownNodeID, ch.ownAddress, ch.contractInstance, ch.contractTransactor, ch.contractCaller.Client
-	connected, remoteSigners, server := ch.connected, ch.remoteSigners, ch.server
+	connected, signers, server := ch.connected, ch.signers, ch.server
 	rsaKey := ch.RsaKey
 	ch.lock.Unlock()
 
 	if !connected {
 		log.Debug("connecting...")
 
-		for _, s := range remoteSigners {
+		for _, s := range signers {
 			err := s.Dial(server, nodeID, address, auth, contractInstance, client, rsaKey)
 			log.Debug("err when connect", "e", err)
 		}
@@ -363,15 +147,15 @@ func (ch *BasicCommitteeHandler) DialAll() {
 }
 
 // Disconnect disconnects all.
-func (ch *BasicCommitteeHandler) Disconnect() {
+func (ch *Handler) Disconnect() {
 	ch.lock.Lock()
-	connected, remoteSigners, server := ch.connected, ch.remoteSigners, ch.server
+	connected, signers, server := ch.connected, ch.signers, ch.server
 	ch.lock.Unlock()
 
 	if connected {
 		log.Debug("disconnecting...")
 
-		for _, s := range remoteSigners {
+		for _, s := range signers {
 			err := s.disconnect(server)
 			log.Debug("err when disconnect", "e", err)
 		}
@@ -382,4 +166,24 @@ func (ch *BasicCommitteeHandler) Disconnect() {
 	ch.lock.Lock()
 	ch.connected = connected
 	ch.lock.Unlock()
+}
+
+// Start starts pbft handler
+func (ch *Handler) Start() error {
+	return nil
+}
+
+// Stop stops all
+func (ch *Handler) Stop() error {
+	return nil
+}
+
+// SendMsg sends msg to signer with given addr
+func (ch *Handler) SendMsg(addr common.Address, msg interface{}) error {
+	return nil
+}
+
+// BroadcastMsg broadcasts msg to all
+func (ch *Handler) BroadcastMsg(msg interface{}) error {
+	return nil
 }
