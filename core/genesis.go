c5a30397de74b00b9d1c184715ad9aec9b653769
xmx0632 <xumx@cpchain.io>
2018-11-13 17:09:03 +0800

    CHAIN-157 delete unused code

--- core/genesis.go
+++ core/genesis.go
@@ -317,18 +317,6 @@ func GenesisBlockForTesting(db database.Database, addr common.Address, balance *
 	return g.MustCommit(db)
 }
 
-// DefaultGenesisBlock returns the Ethereum main net genesis block.
-func DefaultOldGenesisBlock() *Genesis {
-	return &Genesis{
-		Config:     configs.MainnetChainConfig,
-		Nonce:      66,
-		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   5000,
-		Difficulty: big.NewInt(17179869184),
-		Alloc:      decodePrealloc(mainnetAllocData),
-	}
-}
-
 // DefaultGenesisBlock returns the CPChain main net genesis block.
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
