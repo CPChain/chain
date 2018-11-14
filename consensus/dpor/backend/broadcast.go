6dae65e4fda9a69a274e6914d26ae656f69fb8b1
Liu Qian <liuqian@cpchain.io>
2018-11-14 16:46:47 +0800

    CHAIN-239 split handler.go to 3 file

---
+++ consensus/dpor/backend/broadcast.go
@@ -0,0 +1,67 @@
+package backend
+
+import (
+	"time"
+
+	"bitbucket.org/cpchain/chain/commons/log"
+	"bitbucket.org/cpchain/chain/types"
+)
+
+// BroadcastMinedBlock broadcasts generated block to committee
+func (h *Handler) BroadcastMinedBlock(block *types.Block) {
+	committee := h.signers
+	log.Debug("broadcast new generated block to commttee")
+	for addr, peer := range committee {
+		log.Debug("signer", "addr", addr.Hex())
+		peer.AsyncSendNewPendingBlock(block)
+	}
+}
+
+// BroadcastPrepareSignedHeader broadcasts signed prepare header to remote committee
+func (h *Handler) BroadcastPrepareSignedHeader(header *types.Header) {
+	committee := h.signers
+	for _, peer := range committee {
+		peer.AsyncSendPrepareSignedHeader(header)
+	}
+}
+
+// BroadcastCommitSignedHeader broadcasts signed commit header to remote committee
+func (h *Handler) BroadcastCommitSignedHeader(header *types.Header) {
+	committee := h.signers
+	for _, peer := range committee {
+		peer.AsyncSendCommitSignedHeader(header)
+	}
+}
+
+// PendingBlockBroadcastLoop loops to broadcast blocks
+func (h *Handler) PendingBlockBroadcastLoop() {
+	futureTimer := time.NewTicker(10 * time.Second)
+	defer futureTimer.Stop()
+
+	for {
+		select {
+		case pendingBlock := <-h.pendingBlockCh:
+
+			log.Debug("generated new pending block, broadcasting")
+
+			// broadcast mined pending block to remote signers
+			go h.BroadcastMinedBlock(pendingBlock)
+
+		// case <-futureTimer.C:
+
+		// 	// check if still not received new block, if true, continue
+		// 	if h.ReadyToImpeach() {
+
+		// 		// get empty block
+		// 		if emptyBlock, err := h.getEmptyBlockFn(); err == nil {
+
+		// 			// broadcast the empty block
+		// 			h.BroadcastGeneratedBlock(emptyBlock)
+		// 		}
+		// 	}
+
+		case <-h.quitSync:
+			return
+		}
+	}
+}
---
+++ consensus/dpor/backend/dialer.go
@@ -0,0 +1,121 @@
+package backend
+
+import (
+	"context"
+	"math/big"
+
+	"bitbucket.org/cpchain/chain/accounts/abi/bind"
+	"bitbucket.org/cpchain/chain/commons/crypto/rsakey"
+	"bitbucket.org/cpchain/chain/commons/log"
+	contract "bitbucket.org/cpchain/chain/contracts/dpor/contracts/signer_register"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/p2p"
+)
+
+// SetServer sets handler.server
+func (h *Handler) SetServer(server *p2p.Server) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	h.server = server
+	h.ownNodeID = server.Self().String()
+
+	return nil
+}
+
+// SetRsaKey sets handler.rsaKey
+func (h *Handler) SetRsaKey(rsaReader RsaReader) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	var err error
+	h.rsaKey, err = rsaReader()
+
+	return err
+}
+
+// SetContractCaller sets handler.contractcaller.
+func (h *Handler) SetContractCaller(contractCaller *ContractCaller) error {
+
+	// creates an contract instance
+	contractInstance, err := contract.NewSignerConnectionRegister(h.contractAddress, contractCaller.Client)
+	if err != nil {
+		return err
+	}
+
+	// creates a keyed transactor
+	auth := bind.NewKeyedTransactor(contractCaller.Key.PrivateKey)
+
+	gasPrice, err := contractCaller.Client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return err
+	}
+
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = contractCaller.GasLimit
+	auth.GasPrice = gasPrice
+
+	rsaReader := func() (*rsakey.RsaKey, error) {
+		return contractCaller.Key.RsaKey, nil
+	}
+	err = h.SetRsaKey(rsaReader)
+	if err != nil {
+		return err
+	}
+
+	h.lock.Lock()
+
+	// assign
+	h.contractCaller = contractCaller
+	h.contractInstance = contractInstance
+	h.contractTransactor = auth
+
+	h.lock.Unlock()
+
+	return nil
+}
+
+// UpdateSigners updates Handler's signers.
+func (h *Handler) UpdateSigners(epochIdx uint64, signers []common.Address) error {
+	h.lock.Lock()
+	remoteSigners := h.signers
+	h.lock.Unlock()
+
+	for _, signer := range signers {
+		if _, ok := remoteSigners[signer]; !ok {
+			s := NewSigner(epochIdx, signer)
+			remoteSigners[signer] = s
+		}
+	}
+
+	h.lock.Lock()
+	h.epochIdx = epochIdx
+	h.signers = remoteSigners
+	h.lock.Unlock()
+
+	return nil
+}
+
+// DialAll connects remote signers.
+func (h *Handler) DialAll() {
+	h.lock.Lock()
+	nodeID, address, contractInstance, auth, client := h.ownNodeID, h.ownAddress, h.contractInstance, h.contractTransactor, h.contractCaller.Client
+	connected, signers, server := h.dialed, h.signers, h.server
+	rsaKey := h.rsaKey
+	h.lock.Unlock()
+
+	if !connected {
+		log.Debug("connecting...")
+
+		for _, s := range signers {
+			err := s.Dial(server, nodeID, address, auth, contractInstance, client, rsaKey)
+			log.Debug("err when connect", "e", err)
+		}
+		connected = true
+	}
+
+	h.lock.Lock()
+	h.dialed = connected
+	h.lock.Unlock()
+
+}
