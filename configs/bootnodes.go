666b095dd3b02731484ae53782698310a380a339
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-11-15 14:16:52 +0800

    minor

--- configs/bootnodes.go
+++ configs/bootnodes.go
@@ -1,25 +1,11 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2015 The cpchain authors
 
 package configs
 
 // CpchainBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main cpchain network.
 var CpchainBootnodes = []string{
-	"enode://5293dc8aaa5c2fcc7905c21391ce38f4f877722ff1918f4fa86379347ad8a244c2995631f89866693d05bf5c94493c247f02716f19a90689fa406189b03a5243@127.0.0.1:30310", // local
+	"enode://5293dc8aaa5c2fcc7905c21391ce38f4f877722ff1918f4fa86379347ad8a244c2995631f89866693d05bf5c94493c247f02716f19a90689fa406189b03a5243@127.0.0.1:30310", // localhost
 	// "enode://7c4fe440eff5f327ebb6f4f6e688d0a0e037364991f5404f900efc591667f0d0aab4290d73592f594538244b547d1b51e30d34d96038829496002b1894b63da1@54.237.250.178:30311",
 	// "enode://7a04c611ee9f2b982af775dd5da78cec3ebb0505dbecf294c414ac3be27a4ac1c50064e6fe57c78e5e4b62b870214b9ef4063fe0da32a95b2ba78b3ff99b6ce5@34.229.144.205:30312",
 	// "enode://b3351cbfdf4bc9c28bdea2a1e82bcdfdbf177f75841d7b4a93beb1b2e32fe1756786622a2f5dd66d08ddaccb498e746d5aac5a90c0bd6cff771629919f306161@54.152.154.7:30313",
