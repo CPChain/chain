f56b67905730b7aa46b788e33d0e18ef56e4a6b5
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-11-19 15:04:11 +0800

    CHAIN-231 minor

--- api/cpclient/cpclient.go
+++ api/cpclient/cpclient.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package cpchain.provides a client for the Cpchain RPC API.
+// Package cpchain provides a client for the Cpchain RPC API.
 package cpclient
 
 import (
