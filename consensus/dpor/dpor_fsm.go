16f884cb673217864fe79b5a90c39f79cd694447
shiyanchen <shiyc@cpchain.io>
2018-11-27 16:57:33 +0800

    CHAIN-331 added dpor_fsm.go for the validators committee FSM

---
+++ consensus/dpor/dpor_fsm.go
@@ -0,0 +1,82 @@
+package dpor
+
+import (
+	"errors"
+
+	"bitbucket.org/cpchain/chain/types"
+)
+
+//Type enumerator for FSM action
+type action uint8
+
+const (
+	broadcastMsg action = iota
+	insertBlock
+)
+
+//Type enumerator for FSM output
+type dataType uint8
+
+const (
+	header dataType = iota
+	block
+	emptyBlock
+)
+
+//Type enumerator for FSM message type
+type msgCode uint8
+
+const (
+	preprepareMsg msgCode = iota
+	prepareMsg
+	commitMsg
+	validateMsg
+	emtpyPrepareMsg
+)
+
+//Type enumerator for FSM states
+type FsmState uint8
+
+const (
+	idle FsmState = iota
+	preprepared
+	prepared
+	committed
+	inserting
+	impeachmentPreprepared
+)
+
+//verifyBlock is a func to verify whether the block is legal
+func verifyBlock (block *types.Block) bool{
+	return true
+	//TODO: @shiyc
+}
+
+//Fsm is the finite state machine for a validator, to output the correct state given on current state and inputs
+func Fsm(input interface{}, inputType dataType, msg msgCode, state FsmState) (interface{}, action, dataType, msgCode, error) {
+	var inputHeader *types.Header
+	var inputBlock *types.Block
+	var err error
+
+	switch inputType {
+	case header:
+		inputHeader = input.(*types.Header)
+	case block:
+		inputBlock = input.(*types.Block)
+	case emptyBlock:
+		inputBlock = input.(*types.Block)
+	default:
+		err = errors.New("an unexpected input data type"))
+		return nil, 0, 0, 0, err
+	}
+	switch state {
+	case idle:
+		if verifyBlock(inputBlock) {
+
+		}else{
+			err = errors.New("the proposed block is illegal")
+			return *types.Block{}, insertBlock, block, emtpyPrepareMsg, err
+		}
+	}
+	return nil, 0, 0, 0, nil
+}
