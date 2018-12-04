7213798e96cfeca32b56bef46290ab9ffde2c78c
Liu Qian <liuqian@cpchain.io>
2018-12-04 13:48:11 +0800

    CHAIN-325 minor

--- consensus/dpor/backend/validator.go
+++ consensus/dpor/backend/validator.go
@@ -130,6 +130,8 @@ func (vh *Handler) handlePbftMsg(msg p2p.Msg, p *RemoteValidator) error {
 
 	output, act, dtype, msgCode, err := vh.fsm.Fsm(input, inputType, msgCode)
 
+	log.Debug("fsm result", "output", output, "action", act, "data type", dtype, "msg code", msgCode, "error", err)
+
 	// fsm results
 	_, _, _, _, _ = output, act, dtype, msgCode, err
 
