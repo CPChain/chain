6ffc72537cb5da597c00f8c4536e583c1d47ec1a
Liu Qian <liuqian@cpchain.io>
2018-12-05 09:48:30 +0800

    CHAIN-325 changed remote signer

--- consensus/dpor/backend/remote_signer.go
+++ consensus/dpor/backend/remote_signer.go
@@ -33,16 +33,14 @@ type RemoteSigner struct {
 	rw      p2p.MsgReadWriter
 	version int
 
-	term    uint64
 	address common.Address
 
 	lock sync.RWMutex
 }
 
 // NewRemoteSigner creates a new remote signer
-func NewRemoteSigner(term uint64, address common.Address) *RemoteSigner {
+func NewRemoteSigner(address common.Address) *RemoteSigner {
 	return &RemoteSigner{
-		term:    term,
 		address: address,
 	}
 
@@ -56,21 +54,21 @@ func (s *RemoteSigner) Coinbase() common.Address {
 	return s.address
 }
 
-// SetTerm sets term of signer
-func (s *RemoteSigner) SetTerm(term uint64) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+// // SetTerm sets term of signer
+// func (s *RemoteSigner) SetTerm(term uint64) {
+// 	s.lock.Lock()
+// 	defer s.lock.Unlock()
 
-	s.term = term
-}
+// 	s.term = term
+// }
 
-// GetTerm sets term of signer
-func (s *RemoteSigner) GetTerm() uint64 {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
+// // GetTerm sets term of signer
+// func (s *RemoteSigner) GetTerm() uint64 {
+// 	s.lock.RLock()
+// 	defer s.lock.RUnlock()
 
-	return s.term
-}
+// 	return s.term
+// }
 
 // AddStatic adds remote validator as a static peer
 func (s *RemoteSigner) AddStatic(srv *p2p.Server) error {
@@ -111,7 +109,7 @@ func (s *RemoteSigner) SetPeer(version int, p *p2p.Peer, rw p2p.MsgReadWriter) e
 }
 
 // Handshake tries to handshake with remote validator
-func Handshake(p *p2p.Peer, rw p2p.MsgReadWriter, coinbase common.Address, term uint64, verifyProposerFn VerifyFutureSignerFn, verifyValidatorFn VerifyFutureSignerFn) (isProposer bool, isValidator bool, address common.Address, err error) {
+func Handshake(p *p2p.Peer, rw p2p.MsgReadWriter, coinbase common.Address, term uint64, verifyProposerFn VerifySignerFn, verifyValidatorFn VerifySignerFn) (isProposer bool, isValidator bool, address common.Address, err error) {
 	// Send out own handshake in a new thread
 	errc := make(chan error, 2)
 	var signerStatus SignerStatusData // safe to read after two values have been received from errc
@@ -120,12 +118,11 @@ func Handshake(p *p2p.Peer, rw p2p.MsgReadWriter, coinbase common.Address, term 
 		err := p2p.Send(rw, NewSignerMsg, &SignerStatusData{
 			ProtocolVersion: uint32(ProtocolVersion),
 			Address:         coinbase,
-			Term:            term,
 		})
 		errc <- err
 	}()
 	go func() {
-		isProposer, isValidator, address, err = ReadValidatorStatus(p, rw, &signerStatus, verifyProposerFn, verifyValidatorFn)
+		isProposer, isValidator, address, err = ReadValidatorStatus(p, rw, &signerStatus, verifyProposerFn, verifyValidatorFn, term)
 		errc <- err
 	}()
 	timeout := time.NewTimer(handshakeTimeout)
@@ -146,7 +143,7 @@ func Handshake(p *p2p.Peer, rw p2p.MsgReadWriter, coinbase common.Address, term 
 }
 
 // ReadValidatorStatus reads status of remote validator
-func ReadValidatorStatus(p *p2p.Peer, rw p2p.MsgReadWriter, signerStatusData *SignerStatusData, verifyProposerFn VerifyFutureSignerFn, verifyValidatorFn VerifyFutureSignerFn) (isProposer bool, isValidator bool, address common.Address, err error) {
+func ReadValidatorStatus(p *p2p.Peer, rw p2p.MsgReadWriter, signerStatusData *SignerStatusData, verifyProposerFn VerifySignerFn, verifyValidatorFn VerifySignerFn, term uint64) (isProposer bool, isValidator bool, address common.Address, err error) {
 	msg, err := rw.ReadMsg()
 	if err != nil {
 		return false, false, common.Address{}, err
@@ -168,8 +165,8 @@ func ReadValidatorStatus(p *p2p.Peer, rw p2p.MsgReadWriter, signerStatusData *Si
 	// TODO: this (addr, ...) pair should be signed with its private key.
 	// @liuq
 
-	isProposer, err = verifyProposerFn(signerStatusData.Address, signerStatusData.Term)
-	isValidator, err = verifyValidatorFn(signerStatusData.Address, signerStatusData.Term)
+	isProposer, err = verifyProposerFn(signerStatusData.Address, term)
+	isValidator, err = verifyValidatorFn(signerStatusData.Address, term)
 	return isProposer, isValidator, signerStatusData.Address, err
 }
 
