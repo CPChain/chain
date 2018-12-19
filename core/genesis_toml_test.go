eb8294e291ce0b9fdda1c3708075db52532bff43
xmx0632 <xumx@cpchain.io>
2018-12-19 10:47:19 +0800

    CHAIN-392 fix init error

--- core/genesis_toml_test.go
+++ core/genesis_toml_test.go
@@ -17,19 +17,54 @@
 package core
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"testing"
-
-	"fmt"
 
 	"github.com/naoina/toml"
 )
 
 func TestDefaultGenesisBlock_MarshalTOML(t *testing.T) {
 	genesisblock := DefaultGenesisBlock()
+	fmt.Println("==============toml=====================")
+	err := toml.NewEncoder(os.Stdout).Encode(genesisblock)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println("==============json=====================")
+	ss, err := json.Marshal(genesisblock)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println("genesisblock", string(ss))
+}
+
+func TestGenesisAccount_MarshalTOML(t *testing.T) {
+	genesisblock := GenesisAccount{
+		Nonce:      100,
+		Code:       []byte("hhhhh"),
+		PrivateKey: []byte("hhhh2222h"),
+	}
 	fmt.Println("===================================")
 	err := toml.NewEncoder(os.Stdout).Encode(genesisblock)
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestGenesisAccount_MarshalJson(t *testing.T) {
+	genesisblock := GenesisAccount{
+		Nonce:      100,
+		Code:       []byte("hhhhh"),
+		PrivateKey: []byte("hhhh2222h"),
+	}
+	fmt.Println("===================================")
+
+	ss, err := json.Marshal(genesisblock)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println("ss", string(ss))
 }
