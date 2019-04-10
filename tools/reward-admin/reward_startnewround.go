a7f6eb1a27203148cf2a04c9a2852c20f4952732
Changyan LIU <liucy@cpchain.io>
2019-04-10 01:15:20 +0000

    Merged in CHAIN-873-reward-admin-error (pull request #585)
    reward-admin modify error
    
    * reward-admin modify error
    
    * reward-admin compile
    
    * reward-admin cleanup

--- tools/reward-admin/reward_startnewround.go
+++ tools/reward-admin/reward_startnewround.go
@@ -17,7 +17,7 @@ func init() {
 }
 
 func startnewround(ctx *cli.Context) error {
-	admin, out, cancel := build(ctx)
+	admin, out, cancel := Build(ctx)
 	defer cancel()
 	err := admin.StartNewRound()
 	if err != nil {
