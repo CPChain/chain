c1166825ef046b3955fca1c310321a86c2ca968f
shiyanchen <shiyc@cpchain.io>
2019-04-13 15:03:34 +0800

    CHAIN-469 refined conf.py for docs

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = ['sphinx.ext.todo',
               'sphinx.ext.viewcode',
               'sphinx.ext.autodoc',
               #'sphinx.ext.imgmath',
+              'sphinx.ext.doctest',
               'sphinx.ext.mathjax']
 
 
@@ -81,7 +82,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+# html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_rtd_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
