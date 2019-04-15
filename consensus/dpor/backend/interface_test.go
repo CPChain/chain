7a4b35ef65daeb09a577fd7dac677f0253527bb7
jsyqrt <liuqian@cpchain.io>
2019-04-15 17:07:25 +0800

    CHAIN-919 added test cases for IsCheckpoint.

--- consensus/dpor/backend/interface_test.go
+++ consensus/dpor/backend/interface_test.go
@@ -87,3 +87,62 @@ func TestValidMacSig(t *testing.T) {
 		})
 	}
 }
+
+func TestIsCheckPoint(t *testing.T) {
+	type args struct {
+		number  uint64
+		termLen uint64
+		viewLen uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			"succeed",
+			args{
+				12,
+				4,
+				3,
+			},
+			true,
+		},
+		{
+			"fail",
+			args{
+				11,
+				4,
+				3,
+			},
+			false,
+		},
+		{
+			"edge1",
+			args{
+				0,
+				4,
+				3,
+			},
+			false,
+		},
+
+		{
+			"edge2",
+			args{
+				12,
+				4,
+				0,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsCheckPoint(tt.args.number, tt.args.termLen, tt.args.viewLen); got != tt.want {
+				t.Errorf("IsCheckPoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
