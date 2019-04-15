b75dbad9c3daf3470b77773e5ebd4ca28c7e8973
jsyqrt <liuqian@cpchain.io>
2019-04-15 15:26:05 +0800

    CHAIN-921 fixed tx_list AllBefore.

--- core/tx_list_test.go
+++ core/tx_list_test.go
@@ -19,10 +19,14 @@
 package core
 
 import (
+	"math/big"
 	"math/rand"
+	"reflect"
 	"testing"
+	"time"
 
 	"bitbucket.org/cpchain/chain/types"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
@@ -49,5 +53,85 @@ func TestStrictTxListAdd(t *testing.T) {
 		if list.txs.items[tx.Nonce()].Tx() != tx {
 			t.Errorf("item %d: transaction mismatch: have %v, want %v", i, list.txs.items[tx.Nonce()], tx)
 		}
+	}
+}
+
+func generateTxSortedMap(baseTime time.Time) *txSortedMap {
+	size := 20
+	items := make(map[uint64]*TimedTransaction)
+	index := make(nonceHeap, size)
+
+	for i := uint64(0); i < uint64(size); i++ {
+		items[i] = &TimedTransaction{
+			Transaction: types.NewTransaction(i, common.Address{}, big.NewInt(0), uint64(0), big.NewInt(0), []byte{}),
+			updateTime:  baseTime,
+		}
+		index = append(index, i)
+		baseTime = baseTime.Add(time.Second * 10)
+	}
+
+	return &txSortedMap{
+		items: items,
+		index: &index,
+	}
+}
+
+func Test_txSortedMap_AllBefore(t *testing.T) {
+	type fields struct {
+		items map[uint64]*TimedTransaction
+		index *nonceHeap
+		cache types.Transactions
+	}
+	type args struct {
+		t time.Time
+	}
+
+	baseTime, _ := time.Parse(time.RFC3339, "2019-01-01T00:00:00+00:00")
+	fakeTxSortedMap := generateTxSortedMap(baseTime)
+	argTime := baseTime.Add(time.Minute * 1)
+	wantResult := func() []types.Transactions {
+		var results []types.Transactions
+		var result []*types.Transaction
+		for i := 0; i < 6; i++ {
+			result = append(
+				result,
+				types.NewTransaction(uint64(i), common.Address{}, big.NewInt(0), uint64(0), big.NewInt(0), []byte{}),
+			)
+		}
+		results = append(results, result)
+		return results
+	}()
+
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		wantResults []types.Transactions
+	}{
+		// TODO: Add test cases.
+		{
+			"test1",
+			fields{
+				fakeTxSortedMap.items,
+				fakeTxSortedMap.index,
+				fakeTxSortedMap.cache,
+			},
+			args{
+				argTime,
+			},
+			wantResult,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &txSortedMap{
+				items: tt.fields.items,
+				index: tt.fields.index,
+				cache: tt.fields.cache,
+			}
+			if gotResults := m.AllBefore(tt.args.t); !reflect.DeepEqual(gotResults, tt.wantResults) {
+				t.Errorf("txSortedMap.AllBefore() = %v, want %v", gotResults, tt.wantResults)
+			}
+		})
 	}
 }
