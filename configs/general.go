63e2714f12c38b49957061d2a10f512a904233bf
jsyqrt <liuqian@cpchain.io>
2019-04-30 14:41:11 +0800

    CHAIN-987 added rnode number limit in snapshot.go.

--- configs/general.go
+++ configs/general.go
@@ -64,6 +64,11 @@ const (
 	MainnetValidatorsNumber = MainnetFaultyValidatorsNumber*3 + 1
 )
 
+// MaximumRnodeNumber is the max number of rnodes read from campaign contract
+const (
+	MaximumRnodeNumber = 200
+)
+
 const (
 	DefaultDevMaxInitBlockNumber     = 216
 	DefaultTestnetMaxInitBlockNumber = 240
