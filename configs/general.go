026aece6c92d55fdd47be5d7960d21a9b9265441
jsyqrt <liuqian@cpchain.io>
2019-05-13 18:20:26 +0800

    CHAIN-1089 fix campaign 3 pivot number.

--- configs/general.go
+++ configs/general.go
@@ -112,10 +112,10 @@ const (
 const (
 	RptCalcMethod2BlockNumber = 343000
 	RptCalcMethod3BlockNumber = 372400
-	RptCalcMethod4BlockNumber = 388850
+	RptCalcMethod4BlockNumber = 390450
 
 	Campaign2BlockNumber = 371900
-	Campaign3BlockNumber = 388850
+	Campaign3BlockNumber = 390450
 )
 
 var (
