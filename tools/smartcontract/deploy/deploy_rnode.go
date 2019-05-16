fe4572b99f4369cba087ad163697d879a51110f0
Jinlong LIAO <liaojl@cpchain.io>
2019-05-16 06:01:10 +0000

    Merged in CHAIN-1112-smart-contract-tools (pull request #648)
    CHAIN-1112 smart contract tools
    
    * CHAIN-1112 remove proxy pdash and pdash register and pdash proxy and proposer register contracts
    
    * CHAIN-1112 remove unused files and unused code
    
    * CHAIN-1112 replace reward contract with rnode contract
    
    * CHAIN-1112 modify nonce
    
    * update contracts's address

--- tools/smartcontract/deploy/deploy_rnode.go
+++ tools/smartcontract/deploy/deploy_rnode.go
@@ -4,18 +4,18 @@ import (
 	"math/big"
 
 	"bitbucket.org/cpchain/chain/commons/log"
-	pdash "bitbucket.org/cpchain/chain/contracts/pdash/pdash_contract"
+	"bitbucket.org/cpchain/chain/contracts/dpor/rnode"
 	"bitbucket.org/cpchain/chain/tools/smartcontract/config"
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// addr is proxy_register address
-func DeployPdashProxy(password string, nonce uint64, addr common.Address) common.Address {
+// DeployRNode deploy rnode contract
+func DeployRNode(password string, nonce uint64) common.Address {
 	client, err, privateKey, _, fromAddress := config.Connect(password)
 	printBalance(client, fromAddress)
 	// Launch contract deploy transaction.
 	auth := newTransactor(privateKey, new(big.Int).SetUint64(nonce))
-	contractAddress, tx, _, err := pdash.DeployPdashProxy(auth, client, addr)
+	contractAddress, tx, _, err := rnode.DeployRnode(auth, client)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
