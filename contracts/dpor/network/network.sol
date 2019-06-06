5882a011054b6cc567ca80e1997475da16c4e5f9
swliu1994 <liusw@cpchain.io>
2019-06-06 21:00:55 +0800

    network contract and simple test

---
+++ contracts/dpor/network/network.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.24;
+
+
+contract Network {
+    string public host = "8.8.8.8:8888";
+    uint256 public count = 3;
+    uint256 public timeout = 500;
+    uint256 public gap = 100;
+    bool public open = true;
+
+    address owner;
+
+    modifier onlyOwner() {require(msg.sender == owner);_;}
+
+    event UpdateConfig(string config, uint256 value);
+    event UpdateHost(string value);
+    event UpdateOpen(bool value);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function updateHost(string _host) public onlyOwner {
+        host = _host;
+        emit UpdateHost(host);
+    }
+
+    function updateCount(uint256 _count) public onlyOwner {
+        require(_count>=1 && _count<=20);
+        count = _count;
+        emit UpdateConfig("count", count);
+    }
+
+    function updateTimeout(uint256 _timeout) public onlyOwner {
+        require(_timeout>=300 && _timeout<=3000);
+        timeout = _timeout;
+        emit UpdateConfig("timeout", timeout);
+    }
+
+    function updateGap(uint256 _gap) public onlyOwner {
+        require(_gap>=10 && _gap<=3000);
+        gap = _gap;
+        emit UpdateConfig("gap", gap);
+    }
+
+    function updateOpen(bool _open) public onlyOwner {
+        open = _open;
+        emit UpdateOpen(open);
+    }
+}
