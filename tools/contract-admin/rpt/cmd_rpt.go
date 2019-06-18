2941a23e43454846c837e783570a1d4dc93c8380
jsyqrt <liuqian@cpchain.io>
2019-06-18 16:26:35 +0800

    CHAIN-1481 add rpt contract admin

--- tools/contract-admin/rpt/cmd_rpt.go
+++ tools/contract-admin/rpt/cmd_rpt.go
@@ -1,7 +1,13 @@
 package rpt
 
 import (
+	"math/big"
+
+	"bitbucket.org/cpchain/chain/accounts/abi/bind"
+	"bitbucket.org/cpchain/chain/commons/log"
+	rptContract "bitbucket.org/cpchain/chain/contracts/dpor/rpt"
 	"bitbucket.org/cpchain/chain/tools/contract-admin/flags"
+	"bitbucket.org/cpchain/chain/tools/contract-admin/utils"
 	"github.com/urfave/cli"
 )
 
@@ -89,41 +95,193 @@ var (
 )
 
 func setLowRptPercentage(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	pct := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateLowRptPercentage(opts, big.NewInt(pct))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setTotalSeats(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	seats := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateTotalSeats(opts, big.NewInt(seats))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setLowRptSeats(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	seats := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateLowRptSeats(opts, big.NewInt(seats))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setWindow(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	window := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateWindow(opts, big.NewInt(window))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setAlpha(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	alpha := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateAlpha(opts, big.NewInt(alpha))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setBeta(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	beta := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateBeta(opts, big.NewInt(beta))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setGamma(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	gamma := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateGamma(opts, big.NewInt(gamma))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setPsi(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	psi := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdatePsi(opts, big.NewInt(psi))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func setOmega(ctx *cli.Context) error {
+	rpt, opts := createContractInstanceAndTransactor(ctx, true)
+	omega := utils.GetFirstUintArgument(ctx)
+	_, err := rpt.UpdateOmega(opts, big.NewInt(omega))
+	if err != nil {
+		log.Fatal("Failed to update", "err", err)
+	}
+
+	log.Info("Successfully updated")
+
 	return nil
 }
 
 func showConfigs(ctx *cli.Context) error {
+	rpt, _ := createContractInstanceAndTransactor(ctx, false)
+
+	lowPct, err := rpt.LowRptPercentage(nil)
+	if err != nil {
+		log.Fatal("Failed to get low rpt percentage", "err", err)
+	}
+	log.Info("low rpt percentage", "value", lowPct.Int64())
+
+	lowSeats, err := rpt.LowRptSeats(nil)
+	if err != nil {
+		log.Fatal("Failed to get low rpt seats", "err", err)
+	}
+	log.Info("low rpt seats", "value", lowSeats.Int64())
+
+	totalSeats, err := rpt.TotalSeats(nil)
+	if err != nil {
+		log.Fatal("Failed to get total seats", "err", err)
+	}
+	log.Info("total seats", "value", totalSeats.Int64())
+
+	window, err := rpt.Window(nil)
+	if err != nil {
+		log.Fatal("Failed to get window len", "err", err)
+	}
+	log.Info("window len", "value", window.Int64())
+
+	alpha, err := rpt.Alpha(nil)
+	if err != nil {
+		log.Fatal("Failed to get alpha", "err", err)
+	}
+	log.Info("alpha", "value", alpha.Int64())
+
+	beta, err := rpt.Beta(nil)
+	if err != nil {
+		log.Fatal("Failed to get beta", "err", err)
+	}
+	log.Info("beta", "value", beta.Int64())
+
+	gamma, err := rpt.Gamma(nil)
+	if err != nil {
+		log.Fatal("Failed to get gamma", "err", err)
+	}
+	log.Info("gamma", "value", gamma.Int64())
+
+	psi, err := rpt.Psi(nil)
+	if err != nil {
+		log.Fatal("Failed to get psi", "err", err)
+	}
+	log.Info("psi", "value", psi.Int64())
+
+	omega, err := rpt.Omega(nil)
+	if err != nil {
+		log.Fatal("Failed to get omega", "err", err)
+	}
+	log.Info("omega", "value", omega.Int64())
+
 	return nil
+}
+
+func createContractInstanceAndTransactor(ctx *cli.Context, withTransactor bool) (contract *rptContract.Rpt, opts *bind.TransactOpts) {
+	contractAddr, client, key := utils.PrepareAll(ctx, withTransactor)
+
+	if withTransactor {
+		opts = bind.NewKeyedTransactor(key.PrivateKey)
+	}
+
+	contract, err := rptContract.NewRpt(contractAddr, client)
+	if err != nil {
+		log.Fatal("Failed to create new contract instance", "err", err)
+	}
+
+	return contract, opts
 }
