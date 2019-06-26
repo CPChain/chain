9829ef1b01369752cb97ca9d84d8a3bf1b541b68
xmx0632 <xumx@cpchain.io>
2019-06-26 17:47:55 +0800

    CHAIN-1574 add tool to check keystore

---
+++ tools/keystore-checker/keystore_checker.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+
+	"bitbucket.org/cpchain/chain/cmd/cpchain/commons"
+	"bitbucket.org/cpchain/chain/commons/log"
+	"bitbucket.org/cpchain/chain/tools/transfer/config"
+	"github.com/urfave/cli"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "keystore checker"
+	app.Usage = "Try to decrypt keystore with password.\n\t\tExample:./keystore_checker --ks /tmp/keystore/key21"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "keystore, ks",
+			Usage: "Keystore file path for from address",
+			Value: "/tmp/keystore/key1",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		keystorePath := c.String("keystore")
+
+		if !c.IsSet("keystore") {
+			cli.ShowAppHelp(c)
+			return nil
+		}
+
+		// ask for password
+		prompt := "Input password to unlocking account"
+		password, _ := commons.ReadPassword(prompt, false)
+
+		config.SetConfig("", keystorePath)
+		// decrypt keystore
+		_, _, fromAddress, _, _, err := config.DecryptKeystore(password)
+		if err == nil {
+			log.Infof("Decrypt success for : %x", fromAddress)
+		} else {
+			log.Infof("Decrypt failed")
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal("run application failed", "err", err)
+	}
+}
