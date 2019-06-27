aad5bc1939029c336a5f1855d9bf389c2cea41cb
xmx0632 <xumx@cpchain.io>
2019-06-27 10:49:49 +0800

    CHAIN-1504 update mainnet config file

--- core/genesis.go
+++ core/genesis.go
@@ -395,9 +395,9 @@ func newMainnetGenesisBlock() *Genesis {
 			candidates[11]: {Balance: new(big.Int).Mul(big.NewInt(300000), big.NewInt(configs.Cpc))},
 
 			// contract admin account 21
-			common.HexToAddress("0xda5ccb96e93a6a5a80ab1f0a2c5b74ab6a198b45"): {Balance: new(big.Int).Mul(big.NewInt(300000), big.NewInt(configs.Cpc))},
+			common.HexToAddress("0x66961306fd78d39a4604167f59b25697bdad05c5"): {Balance: new(big.Int).Mul(big.NewInt(300000), big.NewInt(configs.Cpc))},
 			// bank 22
-			common.HexToAddress("0x0020511a8d7fd0dc831610a8c2d41c99e2cc9a51"): {Balance: new(big.Int).Mul(big.NewInt(873880028800), big.NewInt(configs.Finney))},
+			common.HexToAddress("0xfc1e61c396c6e02f0dc06bbbe5ee41a97b05b6fd"): {Balance: new(big.Int).Mul(big.NewInt(873880028800), big.NewInt(configs.Finney))},
 		},
 		Dpor: types.DporSnap{
 			Proposers:  configs.Proposers(),
