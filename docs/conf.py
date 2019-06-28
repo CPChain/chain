f40db510f03d62cacb10c111288e54a500bfd298
Jinlong LIAO <liaojl@cpchain.io>
2019-06-28 20:54:01 +0800

    fix bug

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'CPChain'
 # The short X.Y version
 version= os.environ.get('CPC_VERSION', 'latest').replace('v', '')
 # The full version, including alpha/beta/rc tags
-release = f'Mainnet {version}'
+release = 'Mainnet ' + version
 
 
 # -- General configuration ---------------------------------------------------
