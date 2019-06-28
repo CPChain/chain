4c0359e901285e9df96853fe5c778a8ba12b5b5f
Changyan LIU <liucy@cpchain.io>
2019-06-28 07:42:32 +0000

    Merged in CHAIN-1552-auto-generate-doc-version (pull request #788)
    resolve conflict

--- docs/version_modify.sh
+++ docs/version_modify.sh
@@ -4,10 +4,10 @@ echo "[*] Modify conf.py version"
 
 run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-
-
 output=$(python3 $run_dir/version_modify.py 2>&1 >/dev/null)
 echo "[*] The latest version is $output"
+
 sed -i "s/version[ ]*=.*/version= \'$output\' /g" $run_dir/conf.py
+sed -i "s/release[ ]*=.*/release = 'Mainnet Beta $output-doc' /g" $run_dir/conf.py
 
 
