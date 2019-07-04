14db183a557e6f8436df7b520fc8f3baa7a83c64
swliu1994 <liusw@cpchain.io>
2019-07-04 10:13:43 +0800

    CHAIN-1607 test normal case

--- contracts/reward/tests/test_reward.py
+++ contracts/reward/tests/test_reward.py
@@ -34,19 +34,31 @@ def deploy_contract(config, cf, owner):
     return contract_address, reward
 
 
-def generate_nodes(num, cf, owner):
+def generate_nodes(num, cf, owner, value):
     enodes = []
     for i in range(num):
         enode = cf.toChecksumAddress(cf.personal.newAccount("password"))
         enodes.append(enode)
-        cf.cpc.sendTransaction({"from": owner, "to": enode, "value": cf.toWei(30000, "ether")})
+        cf.cpc.sendTransaction({"from": owner, "to": enode, "value": cf.toWei(value, "ether")})
     print("wait for tx confirmation...")
-    time.sleep(15)
+    time.sleep(21)
     for enode in enodes:
         balance = cf.fromWei(cf.cpc.getBalance(enode), "ether")
         print("address: ", enode)
         print("balance: ", balance)
     return enodes
+
+
+def set_period(reward, cf, owner, raise_p, lock_p, settle_p):
+    tx_hash = reward.functions.setRaisePeriod(raise_p).transact({"from": owner, "value": 0})
+    tx_receipt = cf.cpc.waitForTransactionReceipt(tx_hash)
+    print("owner sets raise period, result: ", tx_receipt["status"])
+    tx_hash = reward.functions.setLockPeriod(lock_p).transact({"from": owner, "value": 0})
+    tx_receipt = cf.cpc.waitForTransactionReceipt(tx_hash)
+    print("owner sets lock period, result: ", tx_receipt["status"])
+    tx_hash = reward.functions.setSettlementPeriod(settle_p).transact({"from": owner, "value": 0})
+    tx_receipt = cf.cpc.waitForTransactionReceipt(tx_hash)
+    print("owner sets settlement period, result: ", tx_receipt["status"])
 
 
 def new_raise(cf, reward, owner):
@@ -62,7 +74,7 @@ def fund_bonus_pool(cf, owner, contract_address):
 
 
 def deposit(enode, value, reward):
-    tx_hash = reward.functions.deposit().transact({"from": enode, "value": value})
+    tx_hash = reward.functions.deposit().transact({"from": enode, "value": value * 10**18})
     return tx_hash
 
 
@@ -145,7 +157,92 @@ def monitor(reward, cf):
 
 
 def test_case_1():
-    pass
+    print("#" * 30)
+    print("normal case:\n1. 5 enodes, each 30,000\n2. 5 investors, each 10,000\n"
+          "3. investors withdraw 10,000 during lock, succeed\n"
+          "4. enodes withdraw 10,000 during lock, failed\n"
+          "5. next raise, enodes withdraw money, check value")
+    print("#" * 30)
+    print("[1. compile and deploy contract]")
+    cf, owner = init()
+    config = compile_file()
+    contract_address, reward = deploy_contract(config, cf, owner)
+    print("contract address: ", contract_address)
+    print("[2. initialize enodes and investors]")
+    enodes = generate_nodes(5, cf, owner, 30000)
+    investors = generate_nodes(5, cf, owner, 10000)
+    print("[3. owner sets period for test]")
+    set_period(reward, cf, owner, 1, 1, 1)
+    monitor(reward, cf)
+    print("[4. owner starts a new raise]")
+    new_raise(cf, reward, owner)
+    monitor(reward, cf)
+    print("[5. enodes and investors deposit]")
+    enodes_hashes = []
+    for enode in enodes:
+        tx_hash = deposit(enode, 20000, reward)
+        enodes_hashes.append(tx_hash)
+    investors_hashes = []
+    for investor in investors:
+        tx_hash = deposit(investor, 5000, reward)
+        investors_hashes.append(tx_hash)
+    print("wait for transaction confirmation")
+    time.sleep(21)
+    enode_num = 0
+    for hash in enodes_hashes:
+        if cf.cpc.getTransactionReceipt(hash)["status"]:
+            enode_num += 1
+    print("number of successful enodes: ", enode_num)
+    investor_num = 0
+    for hash in investors_hashes:
+        if cf.cpc.getTransactionReceipt(hash)["status"]:
+            investor_num += 1
+    print("number of successful investors: ", investor_num)
+    monitor(reward, cf)
+    print("[6. owner starts a new lock]")
+    new_lock(cf, reward, owner)
+    monitor(reward, cf)
+    print("[7. investors withdraw money]")
+    investors_hashes = []
+    for investor in investors:
+        free_balance = reward.functions.freeBalanceOf(investor).call()
+        locked_balance = reward.functions.lockedBalanceOf(investor).call()
+        print("address: ", investor)
+        print("free balance: ", cf.fromWei(free_balance, "ether"))
+        print("locked balance: ", cf.fromWei(locked_balance, "ether"))
+        tx_hash = reward.functions.withdraw(free_balance).transact({"from": investor, "value": 0})
+        investors_hashes.append(tx_hash)
+    print("wait for withdraw transaction confirmation")
+    time.sleep(21)
+    investor_num = 0
+    for hash in investors_hashes:
+        if cf.cpc.getTransactionReceipt(hash)["status"]:
+            investor_num += 1
+    print("number of successful investors: ", investor_num)
+    print("[8. owner starts a new settlement and distribute interest]")
+    new_settlement(cf, reward, owner)
+    monitor(reward, cf)
+    print("[9. enodes withdraw money, and check value]")
+    enodes_hashes = []
+    for enode in enodes:
+        free_balance = reward.functions.freeBalanceOf(enode).call()
+        locked_balance = reward.functions.lockedBalanceOf(enode).call()
+        print("address: ", enode)
+        print("free balance: ", cf.fromWei(free_balance, "ether"))
+        print("locked balance: ", cf.fromWei(locked_balance, "ether"))
+        tx_hash = reward.functions.withdraw(free_balance).transact({"from": enode, "value": 0})
+        investors_hashes.append(tx_hash)
+    print("wait for withdraw transaction confirmation")
+    time.sleep(21)
+    enode_num = 0
+    for hash in enodes_hashes:
+        if cf.cpc.getTransactionReceipt(hash)["status"]:
+            enode_num += 1
+    print("number of successful investors: ", enode_num)
+    monitor(reward, cf)
+    print("[10. owner starts a new raise, and check configs]")
+    new_raise(cf, reward, owner)
+    monitor(reward, cf)
 
 
 def test_case_2():
@@ -265,8 +362,8 @@ def test_case_2():
 
 
 def main():
-    # test_case_1()
-    compile_file()
+    test_case_1()
+    # compile_file()
 
 
 if __name__ == '__main__':
