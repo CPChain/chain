ac6bd251d381fa2423342d56889496524649ea7a
Jinlong LIAO <liaojl@cpchain.io>
2021-09-24 10:11:44 +0800

    VAC-1223 refine

--- testnet/list-balance.py
+++ testnet/list-balance.py
@@ -2,8 +2,8 @@
 from cpc_fusion import Web3
 
 def main():
-    target = ['p1', 'p2', 'p3', 'p4', 'ca', 'bank']
-    cf = Web3(Web3.HTTPProvider(f'http://127.0.0.1:8501'))
+    target = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'ca', 'bank']
+    cf = Web3(Web3.HTTPProvider(f'http://127.0.0.1:8499'))
     for i in target:
         with open(f'datadir/{i}/address', 'r') as f:
             addr = f.readline()
@@ -13,3 +13,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# 349 - 
